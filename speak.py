14bcfacfe94cbfcd65b858e9ad214f22d057b360
sasobapriyanjana11 <sasoba.zxc@gmail.com>
2025-02-02 13:32:32 +0530

    implement speak function to understand what we said

---
+++ speak.py
@@ -0,0 +1,11 @@
+import pyttsx3
+def speak(text):
+    engine = pyttsx3.init()
+    rate = engine.getProperty('rate')
+    engine.setProperty('rate', rate-70)
+    engine.say(text)
+    engine.runAndWait()
+
+
+# # should install  # pip3 install pyttsx3
+
