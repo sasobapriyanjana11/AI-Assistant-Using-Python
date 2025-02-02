15c9ba8cf69435fbcbe353aa0d9e5cbefe03b48a
sasobapriyanjana11 <sasoba.zxc@gmail.com>
2025-02-02 09:44:06 +0530

    change colors in Ai Assistant image field and add text widget

--- GUI.py
+++ GUI.py
@@ -6,11 +6,11 @@ root = Tk()
 root.title("AI ASSISTANT")
 root.geometry("550x675")
 root.resizable(False, False)
-root.config(bg="#6F8FAF")
+root.config(bg="#1f6767")
 
 # Main Frame
 Main_frame = LabelFrame(root, padx=100, pady=7, borderwidth=3, relief="raised")
-Main_frame.config(bg="#6F8FAF")
+Main_frame.config(bg="#FFFFFF")
 Main_frame.grid(row=0, column=1, padx=55, pady=10)
 
 # Text Label
@@ -21,6 +21,11 @@ Text_label.grid(row=0, column=0, padx=20, pady=10)
 Display_Image = ImageTk.PhotoImage(Image.open("Image/AI-Assistant.jpeg"))
 Image_Label = Label(Main_frame, image=Display_Image)
 Image_Label.grid(row=1, column=0, pady=20)
+
+# Add a text widget
+text=Text(root , font= ('Courier 10 bold') , bg = "#356696")
+text.grid(row = 2,  column= 0)
+text.place(x= 100, y= 375, width= 375, height= 100) 
 
 
 
