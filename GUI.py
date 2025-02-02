ef2afb9fc1917c2a290bfb5ab02d738e32e3e9b2
sasobapriyanjana11 <sasoba.zxc@gmail.com>
2025-02-02 10:18:55 +0530

    add ask,delete and send buttons and implemet

--- GUI.py
+++ GUI.py
@@ -1,5 +1,31 @@
 from tkinter import *
 from PIL import Image, ImageTk
+import action 
+import spech_to_text 
+
+def User_send():
+    send = entry1.get()
+    bot = action.Action(send)
+    text.insert(END, "Me --> "+send+"\n")
+    if bot != None:
+        text.insert(END, "Bot <-- "+ str(bot)+"\n")
+    if bot == "ok sir":
+          root.destroy()  
+
+def ask():
+
+    ask_val= spech_to_text.spech_to_text()
+    bot_val = action.Action(ask_val)
+    text.insert(END, "Me --> "+ask_val+"\n") 
+    if bot_val != None:
+       text.insert(END, "Bot <-- "+ str(bot_val)+"\n")
+    if bot_val == "ok sir":
+        root.destroy()
+
+def delete_text():
+    text.delete("1.0", "end")
+
+
 
 root = Tk()
 # add title,width,heigh and color before popup
@@ -14,7 +40,7 @@ Main_frame.config(bg="#FFFFFF")
 Main_frame.grid(row=0, column=1, padx=55, pady=10)
 
 # Text Label
-Text_label = Label(Main_frame, text="AI Assistant", font=("Comic Sans MS", 14, "bold"), bg="#356696")
+Text_label = Label(Main_frame, text="AI Assistant", font=("Comic Sans MS", 14, "bold"), bg="#356696", fg="white")
 Text_label.grid(row=0, column=0, padx=20, pady=10)
 
 # Image 
@@ -23,15 +49,26 @@ Image_Label = Label(Main_frame, image=Display_Image)
 Image_Label.grid(row=1, column=0, pady=20)
 
 # Add a text widget
-text=Text(root , font= ('Courier 10 bold') , bg = "#356696")
+text=Text(root , font= ('Courier 10 bold') , bg = "#356696",fg="white", bd=2, relief=RIDGE)
 text.grid(row = 2,  column= 0)
 text.place(x= 100, y= 375, width= 375, height= 100) 
 
 
 #Add Entry widget
-entry1=Entry(root,justify="center")
+entry1=Entry(root,justify="center" ,font=("Comic Sans MS", 12), bg="#FFFFFF", fg="#000000", relief=GROOVE, bd=2)
 entry1.place(x=100,y=500,width=370,height=30)
 
+# Add a text button ask
+button1 =  Button(root,  text="ASK", bg="#356696",fg="white",activebackground="#1f6767",activeforeground="white" ,bd=5, pady=16 ,  padx=40,  borderwidth=3 , relief=SOLID ,  command=ask)
+button1.place(x= 70, y= 575)
+
+# Add a text button send
+button2 =  Button(root,  text="Send" , bg="#356696" , pady=16 ,  padx=40,  borderwidth=3 , relief=SOLID ,  command=User_send)
+button2.place(x= 400, y= 575)
+
+# Add a text button delete text
+button3 = Button(root, text="Delete", bg="#356696" , pady=16 ,  padx=40,  borderwidth=3 , relief=SOLID ,command=delete_text)
+button3.place(x= 225, y= 575)
 
 #popup frame
 root.mainloop()
